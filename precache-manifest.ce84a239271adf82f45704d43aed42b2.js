c83068148a3079cd6d473ed4a067393b6134f168
tejtharang <tharangd95@gmail.com>
2020-03-29 16:28:50 -0500

    Updates

---
+++ precache-manifest.ce84a239271adf82f45704d43aed42b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4005c6f67720b98eaee691a73f05c35a",
+    "url": "/usa-covid-19-tracker/index.html"
+  },
+  {
+    "revision": "a9afd34e9eeb5ff2cfb2",
+    "url": "/usa-covid-19-tracker/static/css/main.0826daad.chunk.css"
+  },
+  {
+    "revision": "86bd81e79c125267a831",
+    "url": "/usa-covid-19-tracker/static/js/2.61da7b50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/usa-covid-19-tracker/static/js/2.61da7b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9afd34e9eeb5ff2cfb2",
+    "url": "/usa-covid-19-tracker/static/js/main.0c39c0cd.chunk.js"
+  },
+  {
+    "revision": "9b9945d27a147bb438a3",
+    "url": "/usa-covid-19-tracker/static/js/runtime-main.c812c7f7.js"
+  }
+]);
